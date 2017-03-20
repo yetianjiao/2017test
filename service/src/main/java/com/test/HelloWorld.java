04145041c3effc966b1a4f8c58a4fef79c431857
yetianjiao <yetianjiaojj@163.com>
2017-03-20 20:34:21 +0800

    master commit and push

--- service/src/main/java/com/test/HelloWorld.java
+++ service/src/main/java/com/test/HelloWorld.java
@@ -11,10 +11,7 @@ public class HelloWorld {
          System.out.println("idea 第二次提交");
          System.out.println("hello world");
          System.out.println("haha");
-<<<<<<<<< Temporary merge branch 1
-          System.out.println("我在github对代码做出修改，知道吗？s");
-=========
           System.out.println("你在github对代码做出修改，我不知道道吗？s");
->>>>>>>>> Temporary merge branch 2
+
      }
 }
