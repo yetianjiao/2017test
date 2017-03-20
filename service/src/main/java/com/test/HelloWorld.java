3f1a34cf141df510a0195e0fbf3c5da17f064375
yetianjiao <yetianjiaojj@163.com>
2017-03-20 17:03:32 +0800

    first from idea

---
+++ service/src/main/java/com/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.Date;
+
+/**
+ * Created by Yetianjiao on 2017/3/20.
+ */
+public class HelloWorld {
+     public static void main(String[] args){
+         //TODO
+         System.out.println(new Date());
+         System.out.println("hello world");
+         System.out.println("haha");
+     }
+}
