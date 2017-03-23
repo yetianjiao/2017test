0e1e6fda463a6636dc985fa41e565de9920ae765
yetianjiao <yetianjiaojj@163.com>
2017-03-23 11:30:42 +0800

    idea  master

--- service/src/main/java/com/test/HelloWorld.java
+++ service/src/main/java/com/test/HelloWorld.java
@@ -6,7 +6,7 @@ import java.util.Date;
  * Created by Yetianjiao on 2017/3/20.
  */
 public class HelloWorld {
-     public static void xx(String[] args){
+     public static void main(String[] args){
          //TODO
          System.out.println("idea 第二次提交");
          System.out.println("hello world");
