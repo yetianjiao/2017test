15346344ecd81b74aa6c410fe4c699ccabfaeb45
yetianjiao <yetianjiaojj@163.com>
2017-03-27 15:02:29 +0800

    修改in eclipse

--- service/src/main/java/com/test/MybatisFirst.java
+++ service/src/main/java/com/test/MybatisFirst.java
@@ -1,25 +1,27 @@
 package com.test;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
 import java.util.List;
 
-import com.test.mapper.UserMapper;
-import com.test.model.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import com.test.mapper.UserMapper;
+import com.test.model.User;
+
 public class MybatisFirst {
-
-
+	
+	//对比直接使JDBC,将mysql配置和sql语句写入配置文件加载，
+	//配置文件的 每一个sql语句对应的是jdbc的statement，id对应唯一的statement
+	//使用session的selectOne，selectList等方法调用配置中“statement”的id来执行curd，也可以封装在接口中，直接调用接口的方法即可
+	
 	// 根据用户名称模糊查询用户列表
 	@Test
 	public void findUserByNameTest() throws Exception {
-		// mybatis配置文件
+		// mybatis配置文件(db链接)
 		String resource = "config/SqlMapConfig.xml";
 		// 得到配置文件流
 		InputStream inputStream = Resources.getResourceAsStream(resource);
@@ -30,7 +32,6 @@ public class MybatisFirst {
 		// 通过工厂得到SqlSession
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		UserMapper mapper=sqlSession.getMapper(UserMapper.class);
-        System.out.println("小明");
 		List<User> list = mapper.findUserByName("小明");
 		System.out.println(list);
 		sqlSession.close();
